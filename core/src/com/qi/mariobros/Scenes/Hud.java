fb4d35b4df6d49a15bd2ac86675880a7b41e2ba2
pqi1 <pqi1@binghamton.edu>
2019-05-11 16:30:59 -0400

    map built up

--- core/src/com/qi/mariobros/Scenes/Hud.java
+++ core/src/com/qi/mariobros/Scenes/Hud.java
@@ -17,15 +17,16 @@ public class Hud {
     private Viewport viewport;
 
     private Integer worldTimer;
+    private boolean timeUp;
     private float timeCount;
-    private Integer score;
+    private static Integer score;
 
-    Label countdownLabel;
-    Label scoreLabel;
-    Label timeLabel;
-    Label levelLabel;
-    Label worldLabel;
-    Label marioLabel;
+    private Label countdownLabel;
+    private static Label scoreLabel;
+    private Label timeLabel;
+    private Label levelLabel;
+    private Label worldLabel;
+    private Label marioLabel;
 
     public Hud(SpriteBatch sb){
         worldTimer = 300;
@@ -40,21 +41,22 @@ public class Hud {
         table.setFillParent(true);
 
         countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel =new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
         table.add(marioLabel).expandX().padTop(10);
-        table.add(worldLabel).expand().padTop(10);
-        table.add(timeLabel).expand().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
         table.row();
         table.add(scoreLabel).expandX();
         table.add(levelLabel).expandX();
         table.add(countdownLabel).expandX();
 
         stage.addActor(table);
+
     }
 
 
--- core/src/com/qi/mariobros/Screens/PlayScreen.java
+++ core/src/com/qi/mariobros/Screens/PlayScreen.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -15,26 +18,52 @@ import com.qi.mariobros.SupMario;
 
 public class PlayScreen implements Screen {
     private SupMario game;
-    Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthoCachedTiledMapRenderer renderer;
 
     public PlayScreen(SupMario game){
         this.game = game;
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(SupMario.V_WIDTH,SupMario.V_HEIGHT,gamecam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthoCachedTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth()/ 2, gamePort.getWorldHeight() / 2, 0);
     }
     @Override
     public void show() {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()){
+            gamecam.position.x += 100 * dt;
+        }
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+
+    }
+
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        update(delta);
+
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
