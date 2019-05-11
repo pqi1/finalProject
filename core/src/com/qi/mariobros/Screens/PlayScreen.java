db7e95ba29470de3bfebee17e25dcd5ec5dd1df6
pqi1 <pqi1@binghamton.edu>
2019-05-11 00:09:33 -0400

    score board on

--- core/src/com/qi/mariobros/Screens/PlayScreen.java
+++ core/src/com/qi/mariobros/Screens/PlayScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.qi.mariobros.Scenes.Hud;
 import com.qi.mariobros.SupMario;
 
 public class PlayScreen implements Screen {
@@ -17,12 +18,13 @@ public class PlayScreen implements Screen {
     Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(SupMario game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(800,400,gamecam);
+        gamePort = new FitViewport(SupMario.V_WIDTH,SupMario.V_HEIGHT,gamecam);
+        hud = new Hud(game.batch);
     }
     @Override
     public void show() {
@@ -33,10 +35,9 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gamecam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
--- core/src/com/qi/mariobros/SupMario.java
+++ core/src/com/qi/mariobros/SupMario.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.qi.mariobros.Screens.PlayScreen;
 
 public class SupMario extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
 	//spritebatch: containner for everything
 	public SpriteBatch batch;
 	
